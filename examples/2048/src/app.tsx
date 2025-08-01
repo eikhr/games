cc327f4d16124824ef8c547fb6365a1304a369c0
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2025-08-01 20:53:25 +0200

    Fix mobile scaling

--- examples/2048/src/app.tsx
+++ examples/2048/src/app.tsx
@@ -2,10 +2,5 @@ import './app.css';
 import { NormalGame } from '2048-preact';
 
 export function App() {
-	return (
-		<>
-			<h1>2048 Demo</h1>
-			<NormalGame />
-		</>
-	);
+	return <NormalGame />;
 }
